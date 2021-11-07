1c760f0a595e79f70ccd4c12c72a8e235e3ad28b
Aviv Yamin <aviv912@gmail.com>
2021-11-07 15:10:24 +0200

    aviv

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const userNameRouter = require("./routers/user-statistics-router");
 app.use(cors());
 app.use(express.json());
 
-app.use("/test", express.static(`./front/dist`));
+app.use("/", express.static(`./front/dist`));
 
 app.get("", (req, res) => {
   res.sendFile(__dirname + "./front/dist/index.html");
