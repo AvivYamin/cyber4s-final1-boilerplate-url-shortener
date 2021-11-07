ff4fd43d4e25b8a1b0ec310645296bc008f04d40
Aviv Yamin <aviv912@gmail.com>
2021-11-07 16:42:06 +0200

    aviv

--- routers/redirect-router.js
+++ routers/redirect-router.js
@@ -4,7 +4,8 @@ const dataManipulator = require("./dataManipulator")
 
 Router.get('/:id', (req, res) => {
     try {
-        const oldUrl = dataManipulator.handleRedirect(`http://localhost:3000/${req.params.id}`);
+        // const oldUrl = dataManipulator.handleRedirect(`http://localhost:3000/${req.params.id}`);
+        const oldUrl = dataManipulator.handleRedirect(`/${req.params.id}`);
         if(oldUrl){
             res.redirect(301, oldUrl);
         }else{
